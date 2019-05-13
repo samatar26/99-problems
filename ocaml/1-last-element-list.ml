4e6591d47012d081172878ddcd0537b251d1d61d
Samatar <samatar_267@hotmail.com>
2019-05-14 01:20:53 +0300

    Finish ocaml exercise 1

---
+++ ocaml/1-last-element-list.ml
@@ -0,0 +1,20 @@
+let rec last (l: 'a list): 'a option = 
+  match l with 
+    [] -> None
+  | [x] -> Some x
+  | _::t -> last t
+
+
+(* 
+Solution
+ *)
+
+let rec last_solution = function 
+  | [] -> None 
+  | [x] -> x 
+  | _::t -> last_solution t
+
+
+(* 
+You can use the function keyword to match on the last argument of a function. No need for match ... with 
+ *)
